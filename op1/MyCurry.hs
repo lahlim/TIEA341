e3ee0ac9e84de38c8a0d3eb8d4c9c825a7fd7425
Lassi <lassi_mustonen@hotmail.com>
2019-09-17 16:30:03 +0300

    Mycurry tehty

---
+++ op1/MyCurry.hs
@@ -0,0 +1,7 @@
+module MyCurry  where
+
+  curry1 :: ((Double, Double) -> Double) -> Double -> Double -> Double
+  curry1 f x y = f (x,y)
+
+  uncurry1 :: (Double -> Double -> Double) -> (Double, Double) -> Double 
+  uncurry1 f (a,b) = f a b
