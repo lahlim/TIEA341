786a3b6c978a72decc8b3f34299dfd6ff878c01b
Lassi <lassi_mustonen@hotmail.com>
2019-10-14 13:22:08 +0300

    dadta constructing aloitettu

---
+++ op2/dataconstructing.hs
@@ -0,0 +1,10 @@
+f1 :: (Int,Char,Bool) -> Maybe Char
+f1 (3, 'a', True) = 'a' 
+{-f2 :: (a,b,c) -> ? b
+f3 :: (a,(b,c,d),e) -> ? c
+f4 :: [a] -> ? a
+f5 :: Either Int String -> ? String
+f6 :: Either a b -> ? b
+g1 :: Maybe a -> b -> ? (Either a b)
+g2 :: a -> b -> ? (a,b)
+g3 :: a -> b -> ? (Either a b)-}
