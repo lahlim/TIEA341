f477bf9aa9a0e0b2ffafd4a8ed36588040424dd9
Lassi <lassi_mustonen@hotmail.com>
2019-10-15 15:38:45 +0300

    ufo2

---
+++ Asteroids-modernwarfare/src/Main.hs
@@ -0,0 +1,207 @@
+module Main where
+  import Graphics.Gloss
+  import Graphics.Gloss.Interface.Pure.Game
+  import Graphics.Gloss.Interface.Pure.Simulate
+  import Graphics.Gloss.Interface.Pure.Display
+  import Graphics.Gloss.Data.Picture
+
+ 
+  data AsteroidWorld = Play [Rock] Ship Ufo [Bullet]
+                     | GameOver 
+                     deriving (Eq,Show)
+  
+  type Velocity     = (Float, Float)
+  type Size         = Float
+  type Age          = Float
+  type Health       = Integer
+  
+  
+  
+  
+  data Ship   = Ship   PointInSpace Velocity      
+      deriving (Eq,Show)
+  data Bullet = Bullet PointInSpace Velocity Age  
+      deriving (Eq,Show)
+  data Rock   = Rock   PointInSpace Size Velocity
+      deriving (Eq,Show)
+  --Change 
+  data Ufo   = Ufo   PointInSpace Velocity Health    
+      deriving (Eq,Show)
+  
+  initialWorld :: AsteroidWorld
+  initialWorld = Play
+                     [Rock (150,150)  45 (2,6)  
+                     ,Rock (-45,201)  45 (13,-8) 
+                     ,Rock (45,22)    25 (-2,8)  
+                     ,Rock (-210,-15) 30 (-2,-8) 
+                     ,Rock (-45,-201) 25 (8,2)  
+                     ] -- The default Rocks
+                     (Ship (0,0) (0,5))
+                     -- The initial ship
+                     (Ufo (120,120) (2,6) 2) -- Ufo --Change
+                     [] -- The initial bullets (none)
+  
+  
+  simulateWorld :: Float -> (AsteroidWorld -> AsteroidWorld)
+  
+  simulateWorld _        GameOver          = GameOver  
+  
+  simulateWorld timeStep (Play rocks (Ship shipPos shipV) (Ufo ufoPos ufoV ufoHealth) bullets)  --Change
+    | any (collidesWith shipPos) rocks  = GameOver
+    | otherwise = Play (concatMap updateRock  rocks) 
+                                (Ship newShipPos shipV)
+                                (updateUfo ( Ufo newUfoPos ufoV ufoHealth)) --Change
+                                (concat (map updateBullet bullets))
+    where
+        collidesWith :: PointInSpace -> Rock -> Bool
+        collidesWith p (Rock rp s _) 
+         = magV (rp .- p) < s
+
+        ufoCollidesWith :: PointInSpace -> Ufo -> Bool
+        ufoCollidesWith p (Ufo rp   _ s) 
+         = magV (rp .- p) < 20
+  
+        collidesWithBullet :: Rock -> Bool
+        collidesWithBullet r 
+         = any (\(Bullet bp _ _) -> collidesWith bp r) bullets
+
+         -- Muutos 2
+        ufoCollidesWithBullet :: Ufo -> Bool
+        
+        ufoCollidesWithBullet r 
+         = let asd = putStrLn "hello, world"
+         in any (\(Bullet bp _ _) -> ufoCollidesWith bp r) bullets  
+
+
+        updateUfo :: Ufo ->  Ufo 
+        updateUfo u@(Ufo p v h) 
+         
+         | ufoCollidesWithBullet u 
+           =  (damageUfo u)
+         | ufoCollidesWithBullet u && h < 1
+           =  u 
+         | otherwise
+           =  u 
+       
+       
+        updateRock :: Rock -> [Rock]
+        updateRock r@(Rock p s v) 
+         | collidesWithBullet r && s < 7 
+              = []
+         | collidesWithBullet r && s > 7 
+              = splitRock r
+         | otherwise                     
+              = [Rock (restoreToScreen (p .+ timeStep .* v)) s v]
+   
+        updateBullet :: Bullet -> [Bullet] 
+        updateBullet (Bullet p v a) 
+          | a > 5                      
+               = []
+          | any (collidesWith p) rocks 
+               = [] 
+          | otherwise                  
+               = [Bullet (restoreToScreen (p .+ timeStep .* v)) v 
+                         (a + timeStep)] 
+  
+        newShipPos :: PointInSpace
+        newShipPos = restoreToScreen (shipPos .+ timeStep .* shipV)
+
+        newUfoPos :: PointInSpace
+        newUfoPos = restoreToScreen (ufoPos .+ timeStep .* ufoV)
+  
+  splitRock :: Rock -> [Rock]
+  splitRock (Rock p s v) = [Rock p (s/2) (3 .* rotateV (pi/3)  v)
+                           ,Rock p (s/2) (3 .* rotateV (-pi/3) v) ]
+
+  damageUfo :: Ufo -> Ufo
+  damageUfo (Ufo p v h) = (Ufo p v (h-1))
+  
+  restoreToScreen :: PointInSpace -> PointInSpace
+  restoreToScreen (x,y) = (cycleCoordinates x, cycleCoordinates y)
+  
+  cycleCoordinates :: (Ord a, Num a) => a -> a
+  cycleCoordinates x 
+      | x < (-400) = 800+x
+      | x > 400    = x-800
+      | otherwise  = x
+  
+  drawWorld :: AsteroidWorld -> Picture
+  
+  drawWorld GameOver 
+     = scale 0.15 0.2 
+       . translate (-400) 0
+       . color red 
+       . text 
+       $ "Game Over! Right Clik to Play!" --Change 1
+  
+  drawWorld (Play rocks (Ship (x,y) (vx,vy)) (Ufo (a,b) (ax,ay) h) bullets) --Change
+    = pictures [ship, asteroids,ufo,shots]
+     where 
+      ship      = color red (pictures [translate x y (ThickCircle 10 20)])
+      ufo      = color green (pictures [translate a b (ThickCircle 20 15)]) --Change
+      asteroids = pictures [translate x y (color orange (ThickCircle s (s/5))) 
+                           | Rock   (x,y) s _ <- rocks]
+      shots     = pictures [translate x y (color violet (circle 2)) 
+                           | Bullet (x,y) _ _ <- bullets]
+  
+  handleEvents :: Event -> AsteroidWorld -> AsteroidWorld
+
+  --Change 2
+  handleEvents (EventKey (MouseButton RightButton) _ _ _) GameOver =  initialWorld
+  handleEvents _ GameOver = GameOver
+
+  
+  
+  
+
+  handleEvents (EventKey (MouseButton LeftButton) Down _ clickPos)
+               (Play rocks (Ship shipPos shipVel) (Ufo ufoPos ufoVel ufoHealth) bullets) --Change
+               = Play rocks (Ship shipPos newVel) (Ufo ufoPos newUfoVel ufoHealth)--Change
+                            (newBullet : bullets)
+   where 
+       newBullet = Bullet shipPos 
+                          (negate 150 .* norm (shipPos .- clickPos)) 
+                          0
+       newVel    = shipVel .+ (50 .* norm (shipPos .- clickPos))
+       -- Tähän ufon siirtymä velozity
+       newUfoVel    = shipVel*0 .+ (0 .* norm (shipPos .- clickPos  )*0) --Change
+  
+  handleEvents _ w = w
+  
+  type PointInSpace = (Float, Float)
+  (.-) , (.+) :: PointInSpace -> PointInSpace -> PointInSpace
+  (x,y) .- (u,v) = (x-u,y-v)
+  (x,y) .+ (u,v) = (x+u,y+v)
+  
+  (.*) :: Float -> PointInSpace -> PointInSpace
+  s .* (u,v) = (s*u,s*v)
+  
+  infixl 6 .- , .+
+  infixl 7 .*
+  
+  norm :: PointInSpace -> PointInSpace
+  norm (x,y) = let m = magV (x,y) in (x/m,y/m)
+  
+  magV :: PointInSpace -> Float
+  magV (x,y) = sqrt (x**2 + y**2) 
+  
+  limitMag :: Float -> PointInSpace -> PointInSpace
+  limitMag n pt = if (magV pt > n) 
+                    then n .* (norm pt)
+                    else pt
+  
+  rotateV :: Float -> PointInSpace -> PointInSpace
+  rotateV r (x,y) = (x * cos r - y * sin r
+                    ,x * sin r + y * cos r)
+  
+  
+  main = play 
+           (InWindow "Asteroids!" (550,550) (20,20)) 
+           black 
+           24 
+           initialWorld 
+           drawWorld 
+           handleEvents
+           simulateWorld
+           
+  
--- op2/dataconstructing.hs
+++ op2/dataconstructing.hs
@@ -1,10 +1,38 @@
-f1 :: (Int,Char,Bool) -> Maybe Char
-f1 (3, 'a', True) = 'a' 
-{-f2 :: (a,b,c) -> ? b
-f3 :: (a,(b,c,d),e) -> ? c
-f4 :: [a] -> ? a
-f5 :: Either Int String -> ? String
-f6 :: Either a b -> ? b
-g1 :: Maybe a -> b -> ? (Either a b)
-g2 :: a -> b -> ? (a,b)
-g3 :: a -> b -> ? (Either a b)-}
+f1 :: (Int,Char,Bool) ->  Char
+f1 (num, letter, bool) = letter 
+
+f2 :: (a,b,c) -> b
+f2 (a,b,c) = case(a,b,c) of  
+      (_,x,_) ->  x
+
+f3 :: (a,(b,c,d),e) -> c
+f3 (a,(b,c,d),e) = c
+
+f4 :: [a] -> Maybe a
+f4 list = case list of
+        [] -> Nothing
+        y:_ -> Just y
+
+f5 :: Either Int String -> String
+f5 muuttuja = case muuttuja of
+            Left _ -> "asd"
+            Right str -> str
+
+f6 :: Either a b -> Maybe b
+f6 ataib = case ataib of
+            Left _ -> Nothing
+            Right b -> Just b
+
+g1 :: Maybe a -> b ->  (Either a b)
+g1 x y = case x of 
+      Nothing -> Right y
+      Just z -> Left z
+
+g2 :: a -> b ->  (a,b)
+g2 a b = (a,b)
+
+g3 :: a -> b ->  (Either a b)
+g3  a b = let 
+          aa = Left a
+          bb = Right b
+          in aa
